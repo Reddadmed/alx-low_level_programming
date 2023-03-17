2e57ac2cca87efcdfdc71780acd7b79bee77924e
reddadmed <reddad007@gmail.com>
2023-03-17 03:00:51 +0000

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -9,7 +9,7 @@
 
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle, followed by a new line.");
+	puts("Programming is like building a multilingual puzzle, followed by a new line.\n");
 	
 	return(0);
 }
