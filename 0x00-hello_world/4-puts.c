01d4a35e543ef232fd7347f9b6704ab7c49c66d9
reddadmed <reddad007@gmail.com>
2023-03-17 02:53:15 +0000

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
-
-
 /**
+ *
  *main - prints "programming is like building a multilingual
- * puzzle, followed by a newline."
+ * puzzle, followed by a new line."
  * return 0.
- * /
+ */
 
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
-	return(0);
+	puts("Programming is like building a multilingual puzzle, followed by a new line.");
+		return(0);
 }
