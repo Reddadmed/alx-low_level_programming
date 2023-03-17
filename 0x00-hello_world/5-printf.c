763da26bafa58ff009159e955322b941f0f59ea0
reddadmed <reddad007@gmail.com>
2023-03-17 21:45:12 +0000

    all

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,10 +3,8 @@
 /**
  * main - prints "with proper grammar, but the outcome is
  * a piece of art," followed newline
- * return: Always 0.
- *
+ * return:0(success);
  */
-
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
